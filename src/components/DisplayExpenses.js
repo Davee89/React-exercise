c9dddc257b193d9f70274490557a60e5923e4e61
Dawid Matras <davematras@gmail.com>
2022-12-21 21:58:03 +0100

    Card added

--- src/components/DisplayExpenses.js
+++ src/components/DisplayExpenses.js
@@ -1,14 +1,15 @@
 import "./DisplayExpenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function DisplayExpenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem title={props.title[0]} amount={props.amount[0]} date={props.date[0]}></ExpenseItem>
       <ExpenseItem title={props.title[1]} amount={props.amount[1]} date={props.date[1]}></ExpenseItem>
       <ExpenseItem title={props.title[2]} amount={props.amount[2]} date={props.date[2]}></ExpenseItem>
       <ExpenseItem title={props.title[3]} amount={props.amount[3]} date={props.date[3]}></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
