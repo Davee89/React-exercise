557498510d5ec3f7322a600b02510774604e0f9d
Dawid Matras <davematras@gmail.com>
2022-12-22 23:27:53 +0100

    Form added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import DisplayExpenses from "./components/Expenses/DisplayExpenses.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
 
 const App = () => {
   const expenses = [
@@ -24,7 +25,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <DisplayExpenses title={expenses.map((expense) => expense.title)} amount={expenses.map((expense) => expense.amount)} date={expenses.map((expense) => expense.date)} />
     </div>
   );
